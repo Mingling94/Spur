fe61c0d70e011cc32a1572edabc6f422bbf05a24
Ryan <ryan@Seans-MacBook-Air.local>
2015-06-06 16:43:57 -0400

    added event for event list

--- components/EventPage/Event.js
+++ components/EventPage/Event.js
@@ -3,14 +3,57 @@
 "use strict";
 
 var React = require('react')
+  , Body = require('./../Body')
+
+var title = {
+
+}
+
+var description = {
+
+}
+
+var time = {
+
+}
+
+var location = {
+
+}
+
+var attendees = {
+
+}
 
 var Event = React.createClass({
 
   render: function() {
     return (
-      <div>
-        Event View
-      </div>
+      <Body>
+      	<a href="/">Events List</a>
+
+        <div style={title}>
+          Event Title
+        </div>
+
+        <div style={description}>
+          This is longer text for the Event Description
+        </div>
+
+        <div style={time}>
+          7:48pm
+        </div>
+
+        <div style={location}>
+          342 Tree Way, Atlanta GA
+        </div>
+
+        <div style={attendees}>
+          List of Attendees: Ryan Britton, Sean Reid...
+        </div>
+
+        
+      </Body>
     );
   }
 
