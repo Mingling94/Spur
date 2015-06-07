40c31a2d4806ca615d5bda22583b69262686cfa5
Sean Reed <sean.reed@threatstack.com>
2015-06-07 10:09:56 -0400

    Whoops

--- components/EventsList/EventsList.js
+++ components/EventsList/EventsList.js
@@ -28,7 +28,7 @@ var EventsList = React.createClass({
 	mixins: [ParseReact.Mixin, Router.Navigation],
 	observe: function() {
 		return {
-			events: EventModel.findEventsWithinN([42.3600825, -71.05888010000001], 1433627757, )
+			events: EventModel.findEventsWithinN([42.3600825, -71.0588801], 1433627757, 5)
 		}
 	},
 	getInitialState: function() {
