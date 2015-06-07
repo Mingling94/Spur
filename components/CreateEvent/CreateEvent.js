2eff546654d334e13bf8c89811041a1868dc6b2f
unknown <mlrawlings@embarkweb.com>
2015-06-07 10:35:17 -0400

    Materialized create event page

--- components/CreateEvent/CreateEvent.js
+++ components/CreateEvent/CreateEvent.js
@@ -6,51 +6,74 @@ var React = require('react')
   , Body = require('./../Body')
   , EventAPI = require('./../../models/events')
   , Moment = require('moment')
+  , UI = require('material-ui')
   // , GMaps = require('react-gmaps') GEO
+
+var styles = {
+  label: {
+    display:'block',
+    textTransform: 'uppercase',
+    fontSize:11,
+    color:'#444'
+  },
+  input: {
+    padding:12,
+    marginBottom:24,
+    width:'100%'
+  },
+  form: {
+    padding:24
+  },
+  action: {
+    float:'right',
+    marginLeft:12
+  }
+}
 
 var Create = React.createClass({
   render: function() {
     return (
       <Body>
-        <a href="/">Back to List</a>
-        Create
-        <form className="createEvent" onSubmit={this.handleSubmit}>
+        <UI.AppBar title="Create a Moment" />
+        <form className="createEvent" onSubmit={this.handleSubmit} style={styles.form}>
           <div>
-            <label>
+            <label style={styles.label}>
               Event Name
             </label>
             <input type="text" name="name" value={this.state.name}
-              onChange={this.updateNameField}/>
+              onChange={this.updateNameField} style={styles.input} />
           </div>
           <div>
-            <label>
+            <label style={styles.label}>
               Location
             </label>
             <input type="text" name="location" value={this.state.location}
-              onChange={this.updateLocationField}/>
+              onChange={this.updateLocationField} style={styles.input} />
           </div>
           <div>
-            <label>
+            <label style={styles.label}>
               Time
             </label>
             <input type="time" name="time" value={this.state.time}
-              onChange={this.updateTimeField}/>
+              onChange={this.updateTimeField} style={styles.input} />
           </div>
           <div>
-            <label>
+            <label style={styles.label}>
               Description
             </label>
-            <textarea onChange={this.updateDescriptionField}>{this.state.description}</textarea>
+            <textarea onChange={this.updateDescriptionField} style={styles.input}>{this.state.description}</textarea>
           </div>
-          <button type="submit">
-            Submit
-          </button>
+          <UI.RaisedButton label="Create" primary={true} style={styles.action} />
+          <UI.RaisedButton label="Cancel" style={styles.action} onClick={this.goBack} />
         </form>
       </Body>
     );
   },
   updateNameField: function(e) {
     this.setState({name: e.target.value});
+  },
+  goBack: function(e) {
+    return false;
   },
   updateLocationField: function(e) {
     var self = this
