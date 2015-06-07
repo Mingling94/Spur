9ebd142734284cf0914ad9bbf39f41aa0c318fb4
Mingling94 <mcluo1994@gmail.com>
2015-06-07 08:09:42 -0400

    join button

--- components/EventPage/EventPage.js
+++ components/EventPage/EventPage.js
@@ -32,42 +32,55 @@ var Event = React.createClass({
   mixins: [Router.State],
   getInitialState: function() {
   	return {
-  		event:{}
+  		event:{
+        attributes: {}
+      }
   	}
   },
   componentDidMount: function() {
   	var self = this
   	EventModel.findEventById(this.getParams().id).then(function(event) {
-  		self.setState({ event: event.attributes })
+  		self.setState({ event: event })
   	})
   },
+  joinEvent: function(e) {
+    console.log(this.state.event)
+    EventModel.addUserToEvent(this.state.event).then(function(result, e) {
+      if (e) {
+        console.log("ERROR " + e)
+      } else {
+        console.log("ASFDJHASDFJHADSKJ")
+      }
+    })
+  },
   render: function() {
-
     return (
       <Body>
       	<a href="/">Events List</a>
 
         <div style={styles.title}>
-          {this.state.event.title}
+          {this.state.event.attributes.title}
         </div>
 
         <div style={styles.description}>
-          {this.state.event.description}
+          {this.state.event.attributes.description}
         </div>
 
         <div style={styles.time}>
-          {this.state.event.timestamp}
+          {this.state.event.attributes.timestamp}
         </div>
 
         <div style={styles.location}>
-          {this.state.event.address}
+          {this.state.event.attributes.address}
         </div>
 
         <div style={styles.attendees}>
-          {this.state.event.attendees}
+          {this.state.event.attributes.attendees}
         </div>
 
-        
+        <button className="join" onClick={this.joinEvent}>
+          Join
+        </button>
       </Body>
     );
   }
