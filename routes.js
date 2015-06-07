48f3aacaac4d512f1fc609a2d0f8e530081662ff
unknown <mlrawlings@embarkweb.com>
2015-06-07 01:00:59 -0400

    require login

--- routes.js
+++ routes.js
@@ -8,10 +8,16 @@ var NotFoundRoute = require('react-router').NotFoundRoute;
 var DefaultRoute = require('react-router').DefaultRoute;
 var Redirect = require('react-router').Redirect;
 var Parse = require('parse').Parse;
+var root = typeof window != 'undefined' ? window : global;
 
 Parse.initialize("lcOvAIMCzFeJKPCOIF4L6IBPQmthkJwShAemMUQ8", "ANHhHgubc9myfbRngBoQQvpFLQdYkw8rsNmKyItN");
 
-
+Parse.FacebookUtils.init({
+    appId      : '1455687261396384', 
+    status     : true, 
+    cookie     : true,
+	version    : 'v2.3'
+});
 
 var Routes = (
   <Route handler={require('./components/App')}>
