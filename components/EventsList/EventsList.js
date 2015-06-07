b620c0a2f53bb59875a46fe1dbbbd27c9a5403c7
unknown <mlrawlings@embarkweb.com>
2015-06-07 06:41:31 -0400

    add material ui elements

--- components/EventsList/EventsList.js
+++ components/EventsList/EventsList.js
@@ -3,50 +3,47 @@
 "use strict";
 
 var React = require('react')
+  , Router = require('react-router')
   , Body = require('./../Body')
   , EventItem = require('./EventItem')
-  , styles = {}
   , EventModel = require('../../models/events')
   , ParseReact = require('parse-react')
+  , UI = require('material-ui')
 
-styles.create = {
-	zIndex: 1,
-	position: 'absolute',
-	right: '1em',
-	bottom: '1em',
-	backgroundColor: 'green',
-	width: '2em',
-	height: '2em',
-	borderRadius: '2em',
-	fontSize: '2em',
-	textAlign: 'center',
-	verticalAlign: 'middle',
-	textDecoration: 'none',
-	color: 'white',
-	padding: '0.5em',
-	lineHeight: 0.9
+var styles = {
+	create: {
+		zIndex: 1,
+		position: 'absolute',
+		right: '1em',
+		bottom: '1em'
+	},
+	icon: {
+		fontSize: '2em',
+		color: 'white'
+	}
 }
-
-
 						
 var EventsList = React.createClass({
-	mixins: [ParseReact.Mixin],
+	mixins: [ParseReact.Mixin, Router.Navigation],
 	observe: function() {
-		var a = EventModel.findEventsWithinN([40.7127837, -74.00594130000002], 1433627757, 5)
-		console.log(a)
 		return {
-			events: a
+			events: EventModel.findEventsWithinN([40.7127837, -74.00594130000002], 1433627757, 5)
 		}
+	},
+	addEvent: function() {
+		this.transitionTo('/event/create')
 	},
 	render: function() {
 		return (
 			<Body>
 				<div>
-					<a style={styles.create} href="/event/create">+</a>
-					<div>Events List</div>
+					<UI.AppBar title="Moments" />
 					{this.data.events.map(function(event) {
 						return <EventItem event={event} />
 					})}
+					<UI.FloatingActionButton onClick={this.addEvent} style={styles.create}>
+						<span style={styles.icon}>+</span>
+					</UI.FloatingActionButton>
 				</div>
 			</Body>
 		)
